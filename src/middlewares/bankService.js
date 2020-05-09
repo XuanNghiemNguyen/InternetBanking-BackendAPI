f53e430c98578bf9a854f66b09cfaf1e29c62bb2
XuanNghiemNguyen <xuanghjem@gmail.com>
2020-05-09 23:53:21 +0700

    fix middlewares

--- src/middlewares/bankService.js
+++ src/middlewares/bankService.js
@@ -5,32 +5,34 @@ const ventureBank = ['VPBank', 'Agribank']
 
 const isPartner = (req, res, next) => {
   try {
-    const partnerCode = req.headers['partner-code']
-    const signature = req.headers['signature']
-    const ts = req.headers['timestamp']
-    const content = req.body
-    if (!partnerCode || !ts || !signature || !content) {
-      throw createError(403, 'missing information. Try again!')
-    }
-    const ts_now = Date.now()
-    if (ts_now > ts && ts_now - ts > 60000) {
-      throw createError(
-        403,
-        'this requirement is not accepted! Data is out of date!'
-      )
-    }
+    const partnerCode = req.headers['partner-code'] || 'default'
+    const signature = req.headers['signature'] || 'default'
+    const ts = req.headers['timestamp'] || 0
+    const content = req.body || 'default'
+
+    //1. A kiểm tra lời gọi api có phải xuất phát từ B (đã đăng ký liên kết từ trước) hay không?
     const bytes = CryptoJS.AES.decrypt(partnerCode, process.env.SERVICE_CODE)
     const bankName = bytes.toString(CryptoJS.enc.Utf8)
-    const sig = md5(ts + content + process.env.SERVICE_CODE)
-
-    if (bankName && ventureBank.includes(bankName) && sig === signature) {
-      req.bankName = bankName
-      next()
-    } else {
-      throw createError(403, 'your bank is not supported!')
+    if (!bankName || !ventureBank.includes(bankName)) {
+      throw createError(401, 'Your bank is not supported!')
     }
+
+    //2. A kiểm tra xem lời gọi này là mới hay là thông tin cũ đã quá hạn?
+    const ts_now = Date.now()
+    if (ts_now < ts || ts_now - ts > 60000) {
+      throw createError(402, 'This request has expired!')
+    }
+
+    //3. A kiểm tra xem gói tin B gửi qua là gói tin nguyên bản hay gói tin đã bị chỉnh sửa?
+    const sig = md5(ts + content + process.env.SERVICE_CODE)
+    if (sig !== signature) {
+      throw createError(403, 'The requested content is no longer intact!')
+    }
+
+    req.bankName = bankName
+    next()
   } catch (error) {
-    return next(createError(403, error.toString()))
+    return next(createError(400, error))
   }
 }
 
