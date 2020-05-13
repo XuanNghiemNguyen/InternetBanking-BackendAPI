4091e577dce2d75caeef58525c18d7999c9741fc
cuongphung09 <43229377+cuongphung09@users.noreply.github.com>
2020-05-13 17:43:03 +0700

    13/5/2020

--- src/routes/service.route.js
+++ src/routes/service.route.js
@@ -1,94 +1,124 @@
-const express = require('express')
-const router = express.Router()
-const Account = require('../models/account')
-const crypto = require('crypto');
-const nodersa = require('node-rsa')
-const fs = require('fs')
-const path = require('path')
-
+const express = require("express");
+const router = express.Router();
+const Account = require("../models/account");
+const crypto = require("crypto");
+const nodersa = require("node-rsa");
+const openpgp = require("openpgp");
+// const passphrase = `12345`;
+const fs = require("fs");
+const path = require("path");
 const publicKey = fs.readFileSync(
-  path.resolve(__dirname + '/../utils/publicKey.pem'),
-  'utf8'
-)
+  path.resolve(__dirname + "/../utils/publicKey.pem"),
+  "utf8"
+);
 const privateKey = fs.readFileSync(
-  path.resolve(__dirname + '/../utils/privateKey.pem'),
-  'utf8'
-)
+  path.resolve(__dirname + "/../utils/privateKey.pem"),
+  "utf8"
+);
 
-// tạo chữ kí truyền vào post man
-const key = new nodersa(publicKey)
+// tạo chữ kí ảo truyền vào post man
+const key = new nodersa(publicKey);
 const encrypted = key.encrypt(
   {
-  "number":"206244691",
-  "amount":"15000"
-  }, 
-  'base64')
-console.log(encrypted)// truyền vào postman encryptedString body
+    number: "206244691",
+    amount: "70000",
+  },
+  "base64"
+);
+console.log(encrypted); // truyền vào postman encryptedString body
 
-
-router.post('/info', async (req, res, next) => {
+router.post("/info", async (req, res, next) => {
   try {
-    const { number } = req.body
-    const account = await Account.findOne({ number })
+    const { number } = req.body;
+    const account = await Account.findOne({ number });
     if (account) {
       return res.json({
         success: true,
         data: {
           number: account.number,
-          balance: account.balance
-        }
-      })
+          balance: account.balance,
+        },
+      });
     }
     return res.json({
       success: false,
-      message: 'account not found!'
-    })
+      message: "account not found!",
+    });
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message: error.toString()
-    })
+      message: error.toString(),
+    });
   }
-})
-router.post('/transfer', async (req, res, next) => {
+});
+router.post("/transfer", async (req, res, next) => {
   try {
-    console.log(req.bankName)
     const partnerKey = fs.readFileSync(
       path.resolve(__dirname + `/../utils/partner-key/${req.bankName}-PublicKey.pem`),
-      'utf8'
-    )
-    const { encryptedString } = req.body
-    const original = new nodersa(privateKey).decrypt(encryptedString, 'utf8')
-    const data = JSON.parse(original)
-    const {number} = data
-    const account = await Account.findOne({ number })
+      "utf8"
+    );
+    const { encryptedString } = req.body;
+    const original = new nodersa(privateKey).decrypt(encryptedString, "utf8");
+    const data = JSON.parse(original);
+    const { number, amount } = data;
+    const account = await Account.findOne({ number });
     if (account) {
-      const respondKey = new nodersa(partnerKey)
-      const encrypted = respondKey.encrypt(
-        {
-          success: true,
-          data: {
-            number: account.number,
-            balance: account.balance
-          }
-        }, 
-        'base64')
+      account.balance = +amount + +account.balance;
+      account.save();
+      const temp = "Agribank";
+      switch (temp) {
+        case "Agribank": {
+          const respondKey = new nodersa(partnerKey);
+          const encrypted = respondKey.encrypt(
+            {
+              success: true,
+              data: {
+                number: account.number,
+                balance: account.balance,
+              },
+            },
+            "base64"
+          );
+          return res.json({
+            encryptedString: encrypted,
+          });
+        }
+        case "Sacombank":
+          const openpgp = require("openpgp");
+          (async () => {
+            await openpgp.initWorker({ path: "openpgp.worker.js" });
+            const publicKeyArmored = partnerKey;
+            const dt = {
+              number: account.number,
+              balance: account.balance,
+            };
+            const { data: encrypted } = await openpgp.encrypt({
+              message: openpgp.message.fromText(JSON.stringify(dt)), // input as Message object
+              publicKeys: (await openpgp.key.readArmored(publicKeyArmored))
+                .keys, // for encryption
+            });
+            return res.json({
+              encryptedString: encrypted,
+            });
+          })();
+          break;
+        default:
+          return res.json({
+            encryptedString: "encrypted",
+          });
+      }
       return res.json({
-        encryptedString: encrypted
-      })
+        encryptedString: "acc not found",
+      });
     }
-    return res.json({
-      success: false,
-      message: 'account not found!'
-    })
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message: error.toString()
-    })
+      message: "error.toString()",
+    });
   }
-})
-module.exports = router
+});
+module.exports = router;
 
 // const input = 'Nguyen Xuan Nghiem - 1612427'
 // const publicKey = fs.readFileSync(
