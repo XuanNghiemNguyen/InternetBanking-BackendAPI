a738043b8a1102201a435346213717b8a1875c89
Cuong <cuongphung09111998@gmail.com>
2020-05-15 10:40:07 +0700

    15/5/2020

--- src/routes/service.route.js
+++ src/routes/service.route.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const Account = require("../models/account");
-const crypto = require("crypto");
 const nodersa = require("node-rsa");
 const openpgp = require("openpgp");
-// const passphrase = `12345`;
+const partnerEncryptMethod = require("../utils/partner-Encrypt-Method")
 const fs = require("fs");
 const path = require("path");
 const publicKey = fs.readFileSync(
@@ -16,16 +15,16 @@ const privateKey = fs.readFileSync(
   "utf8"
 );
 
-// tạo chữ kí ảo truyền vào post man
-const key = new nodersa(publicKey);
-const encrypted = key.encrypt(
-  {
-    number: "206244691",
-    amount: "70000",
-  },
-  "base64"
-);
-console.log(encrypted); // truyền vào postman encryptedString body
+// tạo chữ ký, dữ liệu của đối tác để truyền vào post man
+// const key = new nodersa(publicKey);
+// const encrypted = key.encrypt(
+//   {
+//     number: "206244691",
+//     amount: "70000",
+//   },
+//   "base64"
+// );
+// console.log(encrypted);
 
 router.post("/info", async (req, res, next) => {
   try {
@@ -53,62 +52,53 @@ router.post("/info", async (req, res, next) => {
 });
 router.post("/transfer", async (req, res, next) => {
   try {
-    const partnerKey = fs.readFileSync(
-      path.resolve(__dirname + `/../utils/partner-key/${req.bankName}-PublicKey.pem`),
-      "utf8"
-    );
     const { encryptedString } = req.body;
     const original = new nodersa(privateKey).decrypt(encryptedString, "utf8");
     const data = JSON.parse(original);
     const { number, amount } = data;
     const account = await Account.findOne({ number });
     if (account) {
-      account.balance = +amount + +account.balance;
+      const partnerKey = fs.readFileSync(
+        path.resolve(__dirname + `/../utils/partner-key/${req.bankName}-PublicKey.pem`),
+        "utf8"
+      );
+      account.balance = parseInt(amount) + parseInt(account.balance);
       account.save();
-      const temp = "Agribank";
-      switch (temp) {
-        case "Agribank": {
-          const respondKey = new nodersa(partnerKey);
-          const encrypted = respondKey.encrypt(
-            {
-              success: true,
-              data: {
-                number: account.number,
-                balance: account.balance,
-              },
-            },
-            "base64"
-          );
+      if (partnerEncryptMethod[req.bankName] === 'PGP') {
+        (async () => {
+          await openpgp.initWorker({ path: "openpgp.worker.js" });
+          const publicKeyArmored = partnerKey;
+          const dt = {
+            number: account.number,
+            balance: account.balance,
+          };
+          const { data: encrypted } = await openpgp.encrypt({
+            message: openpgp.message.fromText(JSON.stringify(dt)), // input as Message object
+            publicKeys: (await openpgp.key.readArmored(publicKeyArmored))
+              .keys, // for encryption
+          });
           return res.json({
             encryptedString: encrypted,
           });
-        }
-        case "Sacombank":
-          const openpgp = require("openpgp");
-          (async () => {
-            await openpgp.initWorker({ path: "openpgp.worker.js" });
-            const publicKeyArmored = partnerKey;
-            const dt = {
+        })();
+      } else {
+        const respondKey = new nodersa(partnerKey);
+        const encrypted = respondKey.encrypt(
+          {
+            success: true,
+            data: {
               number: account.number,
               balance: account.balance,
-            };
-            const { data: encrypted } = await openpgp.encrypt({
-              message: openpgp.message.fromText(JSON.stringify(dt)), // input as Message object
-              publicKeys: (await openpgp.key.readArmored(publicKeyArmored))
-                .keys, // for encryption
-            });
-            return res.json({
-              encryptedString: encrypted,
-            });
-          })();
-          break;
-        default:
-          return res.json({
-            encryptedString: "encrypted",
-          });
+            },
+          },
+          "base64"
+        );
+        return res.json({
+          encryptedString: encrypted,
+        });
       }
       return res.json({
-        encryptedString: "acc not found",
+        encryptedString: "Account not found",
       });
     }
   } catch (error) {
---
+++ src/utils/partner-Encrypt-Method.js
@@ -0,0 +1,8 @@
+const partnerEncryptMethod = {
+    agribank: 'PGP',
+    vpbank: 'RSA',
+    eximbank: 'PGP',
+    bidv: 'RSA',
+    ocb: 'PGP'
+  }
+  module.exports = partnerEncryptMethod
