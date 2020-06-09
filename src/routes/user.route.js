676247c06a3ec0dbe8fc750d4dc166b715cfa1de
XuanNghiemNguyen <xuanghjem@gmail.com>
2020-06-09 17:01:07 +0700

    fix: cai dat api

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -8,7 +8,7 @@ router.get('/getListAccount', async (req, res) => {
   try {
     const { email } = req.query
     if (!email) {
-      return res.json({
+      return res.status(400).json({
         success: false,
         message: 'Email is required!'
       })
@@ -31,20 +31,20 @@ router.post('/forgotPassword', async (req, res) => {
     const { userId } = req.tokenPayload
     const { password_1, password_2 } = req.body
     if (!password_1 || !password_2) {
-      return res.json({
+      return res.status(400).json({
         success: false,
         message: 'password_1 and password_2 are required!'
       })
     }
     if (password_1 !== password_2) {
-      return res.json({
+      return res.status(400).json({
         success: false,
         message: 'password_1 and password_2 are not the same!'
       })
     }
     const user = await User.findById(userId)
     if (!user) {
-      return res.json({
+      return res.status(400).json({
         success: false,
         message: 'user not found!'
       })
@@ -55,6 +55,52 @@ router.post('/forgotPassword', async (req, res) => {
       success: true,
       message: 'change password successfully!'
     })
+  } catch (err) {
+    console.log(err)
+    return res.status(500).json({
+      success: false,
+      message: err.toString()
+    })
+  }
+})
+
+router.post('/changePassword', async (req, res) => {
+  try {
+    const { userId } = req.tokenPayload
+    const { password_0, password_1, password_2 } = req.body
+    if (!password_0 || !password_1 || !password_2) {
+      return res.status(400).json({
+        success: false,
+        message: 'password_0, password_1 and password_2 are required!'
+      })
+    }
+    if (password_1 !== password_2) {
+      return res.status(400).json({
+        success: false,
+        message: 'password_1 and password_2 are not the same!'
+      })
+    }
+    const user = await User.findById(userId)
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: 'user not found!'
+      })
+    }
+    const passwordMatching = await bcrypt.compare(password_0, user.password)
+    if (passwordMatching) {
+      user.password = await bcrypt.hash(password_1, 10)
+      await user.save()
+      return res.json({
+        success: true,
+        message: 'change password successfully!'
+      })
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: 'password is incorrect!'
+      })
+    }
   } catch (err) {
     console.log(err)
     return res.status(500).json({
