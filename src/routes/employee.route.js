38bcbd9c5feb4a6facc14484cedb5935743df581
ngô nguyễn <trannguyen3398@.com>
2020-06-18 15:08:54 +0700

    create-user

--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/user')
 const Account = require('../models/account')
+const randtoken = require('rand-token')
+var md5 = require('md5');
 router.get('/deposit',async(req,res)=>{
     try{
         const {stk,amount}=req.body;
@@ -23,5 +25,58 @@ router.get('/deposit',async(req,res)=>{
     })
     }
    
+})
+router.get('/createUser',async(req,res)=>{
+    
+    var {email,phone,name,password,pin}=req.body;
+    password=md5(password);
+    pin=md5(pin);
+    var number;
+    var check=true;
+   
+    
+    while(check!==false){
+      number = Math.floor(Math.random() * 10000000000);
+      check=await Account.findOne({number:number,isEnabled: true})?true:false;
+    }
+    
+    const account = {
+      number: number,
+      balance:0,
+      isPayment: true,
+      isEnabled: true,
+      owner: email,
+      updatedAt:  +new Date() ,
+      createdAt: +new Date() 
+    }
+    
+    const user = {
+    type:  'normal' ,
+    name: name,
+    email: email,
+    password: password,
+    pin:pin,
+    phone: phone,
+    payment: number, //just one payment account
+    savings: [],
+    receivers:  [], 
+    refreshToken: 
+       randtoken.generate(80)
+    ,
+    isEnabled: true,
+    isVerified: true,
+    updatedAt:+new Date(),
+    createdAt: +new Date() 
+  }
+  
+    
+    User.create(user,function(err,res){
+        if (err) throw err;
+       })
+    Account.create(account,function(err,res){
+        if (err) throw err;
+       
+       })
+  res.json({results:"success!"})
 })
 module.exports = router;
