e209f0bfadb93d73fb35e67ae7ae6577f6c4090c
trannguyen33398 <trannguyen3398@gmail.com>
2020-06-23 02:17:00 +0700

    fix deposit, createuser

--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -3,14 +3,18 @@ const router = express.Router()
 const User = require('../models/user')
 const Account = require('../models/account')
 const randtoken = require('rand-token')
-var md5 = require('md5');
-router.get('/deposit',async(req,res)=>{
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+router.post('/deposit',async(req,res)=>{
     try{
-        const {stk,amount}=req.body;
-       
+        const {stk,amount}=req.body
+       if(stk.indexOf('@')==-1){
         const account = await Account.findOne({ number: stk, isEnabled: true })
-       
-      const  updateAccount=await Account.findOneAndUpdate({number:stk},{$set:{balance:account.balance+amount}})
+        var  updateAccount=await Account.findOneAndUpdate({number:stk},{$set:{balance:account.balance+parseFloat(amount)}})
+        } else{
+          const account = await Account.findOne({ owner: stk, isEnabled: true })
+          var  updateAccount=await Account.findOneAndUpdate({number:account.number},{$set:{balance:+account.balance+parseFloat(amount)}})
+        } 
     return res.json({
       success: true,
       results: updateAccount
@@ -28,15 +32,14 @@ router.get('/deposit',async(req,res)=>{
 })
 router.post('/createUser',async(req,res)=>{
     
-    var {email,phone,name,password,pin}=req.body;
-    password=md5(password);
-    pin=md5(pin);
-    var number;
-    var check=true;
+    var {email,phone,name,password,pin}=req.body
+   
+    var number
+    var check=true
    
     
     while(check!==false){
-      number = Math.floor(Math.random() * 10000000000);
+      number= Math.floor(Math.random() * 1000000000) + 1000000000;
       check=await Account.findOne({number:number,isEnabled: true})?true:false;
     }
     
@@ -49,34 +52,38 @@ router.post('/createUser',async(req,res)=>{
       updatedAt:  +new Date() ,
       createdAt: +new Date() 
     }
-    
-    const user = {
-    type:  'normal' ,
-    name: name,
-    email: email,
-    password: password,
-    pin:pin,
-    phone: phone,
-    payment: number, //just one payment account
-    savings: [],
-    receivers:  [], 
-    refreshToken: 
-       randtoken.generate(80)
-    ,
-    isEnabled: true,
-    isVerified: true,
-    updatedAt:+new Date(),
-    createdAt: +new Date() 
-  }
-  
-    
-    User.create(user,function(err,res){
-        if (err) throw err;
-       })
-    Account.create(account,function(err,res){
-        if (err) throw err;
-       
-       })
+    bcrypt.hash(password, saltRounds, (err, hashpass) => {
+      bcrypt.hash(pin, saltRounds, (err, hashpin) => {
+        const user = {
+          type:  'normal' ,
+          name: name,
+          email: email,
+          password: hashpass,
+          pin:hashpin,
+          phone: phone,
+          payment: number, //just one payment account
+          savings: [],
+          receivers:  [], 
+          refreshToken: 
+             randtoken.generate(80)
+          ,
+          isEnabled: true,
+          isVerified: true,
+          updatedAt:+new Date(),
+          createdAt: +new Date() 
+        }
+        
+          
+          User.create(user,function(err,res){
+              if (err) throw err;
+             })
+          Account.create(account,function(err,res){
+              if (err) throw err;
+             
+             })
+      });
+    });
+   
   res.json({results:"success!"})
 })
 module.exports = router;
