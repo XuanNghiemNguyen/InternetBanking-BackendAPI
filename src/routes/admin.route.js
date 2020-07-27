de7ca3aa200e39e6527ca59687523c3bed699399
Cuong <cuongphung09111998@gmail.com>
2020-07-27 17:25:42 +0700

    doing: quan ly nhan vien

--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -10,21 +10,7 @@ const HHBANK_API = require("../services/hhbank")
 const TEAM29_API = require("../services/agribank")
 const { isTrustlyOTP } = require("../middlewares/auth")
 const { updateNotification } = require("../../socket")
-// router.get('/getEmployee', async (req, res) => {
-//     try {
-//         const { email } = req.body
-//         console.log(email)
-//         return res.status(200).json({
-//             success: true,
-//             message: JSON.stringify({ 'abc': 1 }),
-//         })
-//     } catch (error) {
-//         return res.status(500).json({
-//             success: false,
-//             message: err.toString(),
-//         })
-//     }
-// })
+
 router.get("/getEmployee", async (req, res) => {
     try {
         const { email } = req.tokenPayload
@@ -45,7 +31,7 @@ router.get("/getEmployee", async (req, res) => {
         }
         else {
             return res.json({
-                success: true,
+                success: false,
                 results: {
                     success: 'false',
                     message: 'Người dùng không có quyền truy cập'
@@ -54,6 +40,72 @@ router.get("/getEmployee", async (req, res) => {
         }
 
     } catch (err) {
+        console.log(err)
+        return res.status(500).json({
+            success: false,
+            message: err.toString(),
+        })
+    }
+})
+router.post("/editEmployee", async (req, res) => {
+    try {
+        const { data } = req.body
+        if (!data) {
+            return res.status(400).json({
+                success: false,
+                message: 'Employee info is required '
+            })
+        }
+        const emp = await User.findOne({ _id: data._id })
+        emp.name = data.name
+        emp.email = data.email
+        emp.phone = data.phone
+        await emp.save()
+        return res.json({
+            success: true,
+            results: {
+                success: 'true',
+                results: emp
+            },
+        })
+    } catch (error) {
+        console.log(err)
+        return res.status(500).json({
+            success: false,
+            message: err.toString(),
+        })
+    }
+})
+router.post("/lockEmployee", async (req, res) => {
+    try {
+        const { data } = req.body
+        if (!data) {
+            return res.status(400).json({
+                success: false,
+                message: 'Employee info is required '
+            })
+        }
+        const emp = await User.findOne({ _id: data.id })
+        if (emp.isEnabled === true) {
+            emp.isEnabled = false;
+        }
+        else {
+            emp.isEnabled = true;
+        }
+
+
+        // emp.name = data.name
+        // emp.email = data.email
+        // emp.phone = data.phone
+        await emp.save()
+        return res.json({
+            success: true,
+            results: {
+                success: 'true',
+                results: emp
+            },
+        })
+    } catch (error) {
         console.log(err)
         return res.status(500).json({
             success: false,
