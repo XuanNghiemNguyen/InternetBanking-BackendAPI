023f3a4ccf389ba3ba63abcfbb4477c6e7fbe3bc
Cuong <cuongphung09111998@gmail.com>
2020-07-28 16:11:10 +0700

    doing: add employee

--- src/common/index.js
+++ src/common/index.js
@@ -7,5 +7,13 @@ const getRandomCode = () => {
   }
   return ss.join('')
 }
-
-module.exports = { getRandomCode }
+const getRandomPassword = (length) => {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+module.exports = { getRandomCode, getRandomPassword }
--- src/routes/admin.route.js
+++ src/routes/admin.route.js
@@ -10,11 +10,10 @@ const HHBANK_API = require("../services/hhbank")
 const TEAM29_API = require("../services/agribank")
 const { isTrustlyOTP } = require("../middlewares/auth")
 const { updateNotification } = require("../../socket")
-
+const { getRandomCode, getRandomPassword } = require("../common/index")
 router.get("/getEmployee", async (req, res) => {
     try {
         const { email } = req.tokenPayload
-        // console.log(email)
         if (!email) {
             return res.status(400).json({
                 success: false,
@@ -112,5 +111,64 @@ router.post("/lockEmployee", async (req, res) => {
             message: err.toString(),
         })
     }
+})
+
+router.post("/addEmployee", async (req, res) => {
+    var { email, phone, name, password, pin } = req.body.data
+  var number
+  var check = true
+  const isAvailable = (await User.findOne({ email: email })) ? true : false
+  if (isAvailable == true) {
+    res.json({
+      success: false,
+      message: "Email này đã được sử dụng, vui lòng nhập email khác!",
+    })
+  } else {
+    while (check !== false) {
+      number = Math.floor(Math.random() * 1000000000) + 1000000000
+      check = (await Account.findOne({ number: number, isEnabled: true }))
+        ? true
+        : false
+    }
+
+    const account = {
+      number: number,
+      balance: 0,
+      isPayment: true,
+      isEnabled: true,
+      owner: email,
+      updatedAt: Date.now(),
+      createdAt: Date.now(),
+    }
+    bcrypt.hash(password, saltRounds, (err, hashpass) => {
+      bcrypt.hash(pin, saltRounds, (err, hashpin) => {
+        const user = {
+          type: "normal",
+          name: name,
+          email: email,
+          password: hashpass,
+          pin: hashpin,
+          phone: phone,
+          payment: number, //just one payment account
+          savings: [],
+          receivers: [],
+          refreshToken: randtoken.generate(80),
+          isEnabled: true,
+          isVerified: true,
+          updatedAt: Date.now(),
+          createdAt: Date.now(),
+        }
+
+        User.create(user, function (err, res) {
+          if (err) throw err
+        })
+        Account.create(account, function (err, res) {
+          if (err) throw err
+        })
+      })
+    })
+
+    res.json({ results: "success!" })
+  }
 })
 module.exports = router
